7a7930f237231799e3cf21801388d0788090d256
Joaquin Velazquez <joaquin.ve@outlook.com>
2022-01-10 12:58:25 -0600

    Mensaje de prueba

---
+++ index.js
@@ -0,0 +1,13 @@
+// 5 variables 
+//String
+let tipo="Auto electrico";
+//number
+let puertas=5;
+//Boolean
+let motorGasolina=false;
+//undefined
+let aiAssist=undefined;
+//null
+let propTurbo=null;
+
+console.log(`${tipo} con ${puertas}, motor a gasolina: ${motorGasolina}, AIAssist: ${aiAssist} y propulsor turbo: ${propTurbo}`)
